d33cfeb2d1823a750068c19a02e095a339a01552
subhamsingh21 <subhamsingh3579@gmail.com>
2024-09-24 19:49:44 +0530

    findSingleRepeted

---
+++ singleRep.java
@@ -0,0 +1,15 @@
+public class singleRep {
+    public static void findSingleRepeted(int a[],int rep){
+        for(int i=1;i<a.length;i++){
+              rep=rep^a[i];
+
+        }
+        System.out.print("Single element is - "+ rep);
+             
+    }
+    public static void main(String ar[]){
+        int a[]={5,2,3,4,5,3,2};
+        int rep=a[0];
+        findSingleRepeted(a, rep);
+    }
+}
